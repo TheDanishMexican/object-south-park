eff34de7a60f192ec1620d54dc60c3e426ff49ad
TheDanishMexican <danieljchristjansen@gmail.com>
2023-03-27 11:39:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 window.addEventListener("load", start);
 
 async function start() {
-  const southParkPerson = await getCharacter("data/app.json");
-  showCharacter(southParkPerson);
+  const southParkPerson = await getCharacter();
+  showCharacters(southParkPerson);
 }
 
-async function getCharacter(url) {
-  const response = await fetch(url);
+async function getCharacter() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/cederdorff/dat-js/main/05-data/southpark.json"
+  );
   const data = await response.json();
   return data;
 }
@@ -20,22 +22,14 @@ function showCharacters(data) {
 }
 
 function showCharacter(character) {
-  if (character.nickname === null) {
-    const myHTML = /*html*/ `
-     <div><p>Name:<span> ${character.name}</span></p>
-      
-      <p>Occupation:<span> ${character.occupation}</span></p>
-      <img src=${character.image}/>
-      </div>`;
-
-    document
-      .querySelector("#character")
-      .insertAdjacentHTML("beforeend", myHTML);
-    document
-      .querySelector("#character div:last-child")
-      .addEventListener("click", function () {
-        showDialog(character);
-      });
+  if (
+    character.nickname === null ||
+    character.nickname === "undefined" ||
+    character.nickname === undefined ||
+    character.nickname === "Unknown" ||
+    character.nickname === "None"
+  ) {
+    hasNickname(character);
   } else {
     let myHTML = /*html*/ `
      <div><p>Name:<span> ${character.name}</span></p>
@@ -76,4 +70,19 @@ function showDialog(character) {
   document.querySelector("#dialog-firstAppearance").textContent =
     character.firstAppearance;
   document.querySelector("dialog").showModal();
+}
+
+function hasNickname(character) {
+  const myHTML = /*html*/ `
+     <div><p>Name:<span> ${character.name}</span></p>
+      <p>Occupation:<span> ${character.occupation}</span></p>
+      <img src=${character.image}/>
+      </div>`;
+
+  document.querySelector("#character").insertAdjacentHTML("beforeend", myHTML);
+  document
+    .querySelector("#character div:last-child")
+    .addEventListener("click", function () {
+      showDialog(character);
+    });
 }
